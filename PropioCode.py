f0d1ca80e61c2a4dd8d07554f9c22b4cc324ca18
XxCesarinxX <66038328+XxCesarinxX@users.noreply.github.com>
2021-01-28 20:39:27 -0600

    rechulo

--- PropioCode.py
+++ PropioCode.py
@@ -13,9 +13,15 @@ def busqueda():
     search.send_keys(busquedas, Keys.ARROW_DOWN)
     time.sleep(1)
     search.send_keys(Keys.ENTER)
-    time.sleep(2)
+    time.sleep(3)
     resultados = driver.find_elements_by_tag_name("h3")
-    print(resultados)
+    ca = 0
+    for numero in resultados:
+        ca= ca + 1
+    print("paguinas obtenidas", ca)
+    paguina = int(input("que paguina desea utilizar\n\t-->"))
+    resultados[paguina].click()
+    time.sleep(1)
 
 def cambio_ventna():
     driver.execute_script("window.open('');")
@@ -23,10 +29,10 @@ def cambio_ventna():
 
 def resoomer():
     driver.execute_script("window.open('');")
-    driver.switch_to.window(driver.window_handles[2])
+    driver.switch_to.window(driver.window_handles[1])
     driver.get("https://resoomer.com/es/")
     importar = driver.find_element_by_xpath("/html/body/section[1]/div/div[2]/div[1]/div/form/fieldset/div/textarea")
-    importar.send_keys(informacion)
+    importar.send_keys(url)
     time.sleep(2)
     button = driver.find_element_by_xpath("/html/body/section[1]/div/div[2]/div[1]/div/form/fieldset/div/div[1]/div/ul/li[2]/input")
     button.click()
@@ -37,4 +43,5 @@ print("Que es lo que desa resumir el dia de hoy, galan")
 busquedas = input()
 driver = webdriver.Firefox(executable_path=r"C:\Users\cesar\Documents\geckodriver.exe")
 busqueda()
-time.sleep(2)
+url = driver.current_url
+resoomer()
